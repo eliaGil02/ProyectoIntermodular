7d39eff53a82d0267f7a31101d7a5c84e96a47e8
Daniel Garcia Mora <danielgm.daw@gmail.com>
2026-04-09 09:47:37 +0200

    db: actualizo el esquema de base de datos: añadida tabla 'usuarios' con campos para realizar el login y roles (alumno/profesor), añadido campo 'usuario_id' en la tabla 'triajes' para que nos diga quien realizó el triaje y la profesora pueda evaluar el trabajo de sus alumno, actualizadas las claves foráneas.

--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,16 @@ DROP DATABASE IF EXISTS triaje_app;
 CREATE DATABASE triaje_app;
 USE triaje_app;
 
+-- TABLA USUARIOS
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    nombre VARCHAR(100),
+    rol ENUM('alumno','profesor') NOT NULL,
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
 -- TABLA PACIENTES
 CREATE TABLE pacientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -39,11 +49,14 @@ CREATE TABLE triajes (
     
     peso DECIMAL(5,2),
     talla DECIMAL(5,2),
+
+    usuario_id INT, -- Nos va a ayudar a saber quién hizo el triaje, para que la profesora pueda evaluar el trabajo de cada alumno
     
     categoria ENUM('Rojo','Naranja','Amarillo','Verde','Azul'),
     flujo ENUM('RCP','Nivel I','Nivel II','Traumatologia','Obstetrica','Pediatria','Psiquiatria'),
     
     FOREIGN KEY (paciente_id) REFERENCES pacientes(id));
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 
 
 -- TABLA ATENCION
